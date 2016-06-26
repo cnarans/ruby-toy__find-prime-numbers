c11a8d281b37251c1ceb683ee68fda7b21f09309
Colin Narans <cnarans@gmail.com>
2016-06-25 20:38:51 -0500

    working prime

--- lib/find_prime_numbers.rb
+++ lib/find_prime_numbers.rb
@@ -2,4 +2,20 @@
 # returns a properly formatted phone number.
 
 def find_prime_numbers(num)
+	primes = [];
+	for i in 0..num
+		primes.push(i)
+	end
+	primes[1]=0
+	for i in 2..num
+		if primes[i]!=0
+			j=i*2
+			while j<=num
+				primes[j]=0
+				j+=i
+			end
+		end
+	end
+	primes.delete(0)
+	return primes
 end
